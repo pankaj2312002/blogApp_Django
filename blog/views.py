5e24cd07dfaaa462581b590bc0e5f8775a3023b8
ghanshyam kumawat <ghanshyaminvent@gmail.com>
2026-06-03 14:36:17 +0530

    Intial Commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+# from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    print(post)
+    return render(request, 'blog/post_detail.html', {'post': post})
---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+"""mysite URL Configuration
+
+[...]
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
